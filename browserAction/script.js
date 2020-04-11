3cbfcc61e493faf4a31dedcd658fa879d0d77f6a
d02d33pak <deepak030190@gmail.com>
2020-04-12 02:17:43 +0530

    api calling and fetching definition

--- browserAction/script.js
+++ browserAction/script.js
@@ -1,8 +1,41 @@
+function getDefinition(keyword) {
+    var apiKey = "";
+    var reqURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/";
+    reqURL += keyword;
+    reqURL += "?key=" + apiKey; 
+
+    // GET request
+    var Http = new XMLHttpRequest();
+    Http.open("GET", reqURL, true);
+    Http.send(null);
+    // handle response
+    Http.onreadystatechange = function () {
+        if (Http.readyState == 4 && Http.status == 200) {
+            var resultText = document.getElementById("text-result");
+            resultText.innerHTML = "";
+            var responseJSON = JSON.parse(Http.responseText);
+            results = responseJSON[0].def[0].sseq;
+            results.forEach(function (item) {
+                item.forEach(function (definition) {
+                    definition.forEach(function (meaning) {
+                        if (meaning.dt) {
+                            console.log(meaning.dt[0][1]);
+                            resultText.innertHTML += JSON.stringify(meaning.dt[0][1]);
+                        }
+                    });
+                });
+            });
+        }
+    }    
+
+}
+
 function handleResponse(message) {
     var keyWord = document.getElementById("keyword");
     if ((message.response).length > 0) {
         // change text in pop-up
         keyWord.innerHTML = message.response;
+        getDefinition(message.response);
     }
 }
 
