7ab401e59519b8684314cc2fdfb3989dda5f93ee
Atish Mishra <atish.mishra@blueorca.co>
2020-04-12 05:34:44 +0530

    deleted background scripts

--- background-script.js
+++
@@ -1,7 +0,0 @@
-function handleMessage(request, sender, sendResponse) {
-  sendResponse({
-    response: 'Response from background script' + request.selectedText,
-  });
-}
-
-browser.runtime.onMessage.addListener(handleMessage);
