28cc9be233b6c6fd81d0af3e59088dc48b294e21
Atish Mishra <atish.mishra@blueorca.co>
2020-04-12 05:33:11 +0530

    refactored code and showing definitions as lists

--- browserAction/script.js
+++ browserAction/script.js
@@ -1,64 +1,56 @@
-function getDefinition(keyword) {
-    var apiKey = "";
-    var reqURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/";
-    reqURL += keyword;
-    reqURL += "?key=" + apiKey; 
-
-    // GET request
-    var Http = new XMLHttpRequest();
-    Http.open("GET", reqURL, true);
-    Http.send(null);
-    // handle response
-    Http.onreadystatechange = function () {
-        if (Http.readyState == 4 && Http.status == 200) {
-            var resultText = document.getElementById("text-result");
-            resultText.innerHTML = "";
-            var responseJSON = JSON.parse(Http.responseText);
-            results = responseJSON[0].def[0].sseq;
-            results.forEach(function (item) {
-                item.forEach(function (definition) {
-                    definition.forEach(function (meaning) {
-                        if (meaning.dt) {
-                            console.log(meaning.dt[0][1]);
-                            resultText.innertHTML += JSON.stringify(meaning.dt[0][1]);
-                        }
-                    });
-                });
-            });
-        }
-    }    
-
+function getDefinition(keyword, callback) {
+  const apiKey = '';
+  const reqURL = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${keyword}?key=${apiKey}`;
+  // GET request
+  const Http = new XMLHttpRequest();
+  Http.open('GET', reqURL, true);
+  Http.send(null);
+  // handle response
+  Http.onreadystatechange = () => {
+    if (Http.readyState == 4 && Http.status == 200) {
+      const data = JSON.parse(Http.responseText);
+      callback(data);
+    }
+  };
 }
 
 function handleResponse(message) {
-    var keyWord = document.getElementById("keyword");
-    if ((message.response).length > 0) {
-        // change text in pop-up
-        keyWord.innerHTML = message.response;
-        getDefinition(message.response);
-    }
+  let keyword = document.getElementById('keyword');
+  let resultText = document.getElementById('text-result');
+  if (message.response.length > 0) {
+    keyword.innerHTML = message.response;
+    resultText.innerHTML = '';
+
+    getDefinition(message.response, (responseJSON) => {
+      results = responseJSON[0].def[0].sseq;
+      let ul = document.createElement('ul');
+      results.forEach((item) => {
+        let definitionText = item[0][1]['dt'][0][1];
+        let li = document.createElement('li');
+        ul.appendChild(li);
+        li.innerHTML += definitionText;
+      });
+      resultText.appendChild(ul);
+    });
+  }
 }
 
 function handleError(error) {
-    console.log(`Error: ${error}`);
+  console.log(`Error: ${error}`);
 }
 
-function notifyContentScript() {
-    browser.tabs.query(
-        {
-            active: true,
-            currentWindow: true
-        },
-        function (tabs) {
-            var sender = browser.tabs.sendMessage(
-            tabs[0].id,
-            {
-                from: "browserAction",
-                msg : "getText"
-            });
-            sender.then(handleResponse, handleError);
-        }
-    );
-}
-
-notifyContentScript();
+(() => {
+  browser.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    function (tabs) {
+      var sender = browser.tabs.sendMessage(tabs[0].id, {
+        from: 'browserAction',
+        msg: 'getText',
+      });
+      sender.then(handleResponse, handleError);
+    },
+  );
+})();
