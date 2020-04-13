a2d28bda30f52d1190187b82ebe12b7516a778ae
d02d33pak <deepak030190@gmail.com>
2020-04-13 23:52:02 +0530

    function for a_link

--- browserAction/util.js
+++ browserAction/util.js
@@ -1,15 +1,21 @@
 function formatMeaning(meaning) {
-  meaning = meaning.replace(/{bc}/g, '');
-  meaning = meaning.replace(/{ldquo}/g, '"');
-  meaning = meaning.replace(/{rdquo}/g, '"');
-  meaning = meaning.replace(/{b}/g, '<b>');
-  meaning = meaning.replace(/{\/b}/g, '</b>');
-  meaning = meaning.replace(/{inf}/g, '<sub>');
-  meaning = meaning.replace(/{\/inf}/g, '</sub>');
-  meaning = meaning.replace(/{sup}/g, '<sup>');
-  meaning = meaning.replace(/{\/sup}/g, '</sup>');
-  meaning = meaning.replace(/{it}/g, '<i>');
-  meaning = meaning.replace(/{\/it}/g, '</i>');
+  meaning = meaning.replace(/{bc}/g, '');           // bold colon and a space
+  meaning = meaning.replace(/{ldquo}/g, '"');       // double quotes open
+  meaning = meaning.replace(/{rdquo}/g, '"');       // double quotes close
+  meaning = meaning.replace(/{b}/g, '<b>');         // bold open
+  meaning = meaning.replace(/{\/b}/g, '</b>');      // bold close
+  meaning = meaning.replace(/{it}/g, '<i>');        // italics open
+  meaning = meaning.replace(/{\/it}/g, '</i>');     // italics close
+  meaning = meaning.replace(/{inf}/g, '<sub>');     // subscript open
+  meaning = meaning.replace(/{\/inf}/g, '</sub>');  // subscript close
+  meaning = meaning.replace(/{sup}/g, '<sup>');     // superscript open
+  meaning = meaning.replace(/{\/sup}/g, '</sup>');  // superscript close
+  meaning = meaning.replace(/{a_link\|\w+}/g, replace_alink);
+
+  function replace_alink(match) {
+    console.log(match);
+    return match.slice(8, -1);
+  }
 
   return meaning;
 }
